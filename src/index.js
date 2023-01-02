ce66146e0c0ce42662a7413fea4e2efeb72208ac
Egor Kiprin <kiprin.egor@yandex.ru>
2023-01-02 16:58:34 +0700

    pageup on Jquery

--- src/index.js
+++ src/index.js
@@ -10,5 +10,33 @@ const root = document.querySelector('#root')
 root.append(heading)
 const img = new Image();
 
+// const anchors = document.querySelectorAll('a.menu_link')
 
+// for (let anchor of anchors) {
+//     anchor.addEventListener('click', function (e) {
+//     e.preventDefault()
+    
+//     const blockID = anchor.getAttribute('href')
+    
+//     document.querySelector(blockID).scrollIntoView({
+//         behavior: 'smooth',
+//         block: 'start'
+//     })
+//     })
+// }
+// Smooth scroll and pageup
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1000) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
 
