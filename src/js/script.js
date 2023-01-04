0c19b4ff950efd8d6146ca8c4189616be76cabff
Egor Kiprin <kiprin.egor@yandex.ru>
2023-01-04 21:03:33 +0700

    some minor changes

--- src/js/script.js
+++ src/js/script.js
@@ -15,24 +15,34 @@
 // });
 
 //JS
-
-document.querySelectorAll('a[href^="#"').forEach(link => {
-
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        let href = this.getAttribute('href').substring(1);
-
-        const scrollTarget = document.getElementById(href);
-
-        const topOffset = document.querySelector('.experience').offsetHeight;
-        // const topOffset = 0; // если не нужен отступ сверху 
-        const elementPosition = scrollTarget.getBoundingClientRect().top;
-        const offsetPosition = elementPosition - topOffset;
-
-        window.scrollBy({
-            top: offsetPosition,
-            behavior: 'smooth'
-        });
-    });
+// добавляем скрипт, который покажет стрелку, если мы опукаемся вниз экрана.
+document.addEventListener("scroll", (event) => {
+    if (window.scrollY > 1000) {
+        document.querySelector('.pageup').style.display = "block";
+    } else {
+        document.querySelector('.pageup').style.display = "none";
+    }
 });
+
+// document.querySelectorAll('a[href^="#"').forEach(link => {
+
+//     link.addEventListener('click', function(e) {
+//         e.preventDefault();
+
+//         let href = this.getAttribute('href').substring(1);
+//         console.log(this.getAttribute('href'))
+        
+
+//         const scrollTarget = document.getElementById(href);
+
+//         const topOffset = document.querySelector('.experience').offsetHeight;
+//         // const topOffset = 0; // если не нужен отступ сверху 
+//         const elementPosition = scrollTarget.getBoundingClientRect().top;
+//         const offsetPosition = elementPosition - topOffset;
+
+//         window.scrollBy({
+//             top: offsetPosition,
+//             behavior: 'smooth'
+//         });
+//     });
+// });
