bf5dfb592b3c3fcb1c543213b9e8f3241ab96fe3
Egor Kiprin <kiprin.egor@yandex.ru>
2023-03-02 10:57:40 +0700

    some minor changes

--- src/js/script.js
+++ src/js/script.js
@@ -39,3 +39,26 @@ if (window.scrollY > 1000) {
 //         document.querySelector('.pagedown').style.display = "none";
 //     }
 //     });
+
+// //<script>
+// // поместить все текстовые узлы в элемент <span>
+// // он занимает только то место, которое необходимо для текста
+// for (let li of tree.querySelectorAll('li')) {
+//     let span = document.createElement('span');
+//     li.prepend(span);
+//     span.append(span.nextSibling); // поместить текстовый узел внутрь элемента <span>
+//     }
+
+//     //  ловим клики на всём дереве
+//     tree.onclick = function(event) {
+
+//     if (event.target.tagName != 'SPAN') {
+//         return;
+//     }
+
+//     let childrenContainer = event.target.parentNode.querySelector('ul');
+//     if (!childrenContainer) return; // нет детей
+
+//     childrenContainer.hidden = !childrenContainer.hidden;
+// }
+// //</script>
